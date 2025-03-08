eb0cfbe2e0d1e9b2b30c95c88a6d908d75ec735c
n3g3ntr0pe <po.box.8008@gmail.com>
2025-03-08 10:37:59 +1000

    Create components directory with placeholder file

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// This file serves as a placeholder to ensure the components directory is created
+// It will also serve as an export point for components
+
+// Example exports (uncomment as components are created)
+// export { default as LoadingIndicator } from './LoadingIndicator';
+// export { default as SimpleAllocationTreemap } from './SimpleAllocationTreemap';
+// export { default as RiskAnalysisView } from './RiskAnalysisView';
+// export { default as SimpleTimeSeriesView } from './SimpleTimeSeriesView';
